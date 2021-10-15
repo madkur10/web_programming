6d7c4e9168ae8b54dde4b641f38ad133cc4e5269
madkur10 <blackmadkur@gmail.com>
2021-10-15 14:03:13 +0700

    First Commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Muhammad Kurniawan
+            Saya tinggal di daerah Meruya
+            Olahraga yang saya sukai adalah
+            Futsal";
+    }
+}
